3e9cba3214a5340e56cd99392e6201d010aaa5cd
yegawa <yegawa@esm.co.jp>
2026-07-14 14:43:52 +0900

    ソース修正（再開時２）

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,47 +1,56 @@
 import type { User } from "../types/auth";
 
 // const BASE_URL = "/api/auth";
-const BASE_URL = "http://localhost:3000/api/auth";
+const BASE_URL = "/api/auth";
 
 // ログイン時
-export const login = async (user_name: string, user_password: string): Promise<{ message: string; user?: User }> => {
-  const res = await fetch(`${BASE_URL}/login`,{
+export const login = async (
+  useremail: string,
+  userpassword: string,
+): Promise<{ message: string; user?: User }> => {
+  // ログインデータを送信
+  const res = await fetch(`${BASE_URL}/login`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ user_name, user_password }),
+    body: JSON.stringify({
+      user_email: useremail,
+      user_password: userpassword,
+    }),
   });
+
+  // レスポンスチェック
   if (!res.ok) {
     throw new Error(`Failed to login: ${res.status}`);
   }
+
+  // JSONデータとして返却
   return res.json() as Promise<{ message: string; user?: User }>;
 };
 
 // 新規登録時
-export const signup = async (user_name: string, user_password: string, monthly_budget: number)
-  : Promise<{ message: string; user?: User }> => {
-  const res = await fetch(`${BASE_URL}/signup`,{
+export const signup = async (
+  userName: string,
+  userPassword: string,
+  userEmail: string,
+  budget: number | null,
+): Promise<{ message: string; user?: User }> => {
+  // 新規登録データを送信
+  const res = await fetch(`${BASE_URL}/signup`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ user_name, user_password, monthly_budget }),
+    body: JSON.stringify({
+      user_name: userName,
+      user_password: userPassword,
+      user_email: userEmail,
+      monthly_budget: budget,
+    }),
   });
+
+  // レスポンスチェック
   if (!res.ok) {
     throw new Error(`Failed to sign-up: ${res.status}`);
   }
+
+  // JSONデータとして返却
   return res.json() as Promise<{ message: string; user?: User }>;
-};
-
-// 月額設定額登録
-export const updateBudget = async (user_id: number, monthly_budget: number)
-  : Promise<{ message: string; monthly_budget: number}> => {
-  const res = await fetch(`${BASE_URL}/update-budget`,{
-    method: "PATCH",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ user_id, monthly_budget }),
-  });
-
-  if (!res.ok) {
-    throw new Error(`Failed to update budget: ${res.status}`);
-  }
-
-  return res.json() as Promise<{ message: string; monthly_budget: number }>;
 };
